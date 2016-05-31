e82f144bb4a1081f8f275db2c8112f5b29b99ccb
Carolyn Huynh <ckhuynh20@gmail.com>
2016-05-31 12:39:10 -0700

    initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+		// if the left button is down
+		if (Input.GetKey ("left")) {
+			// move ball to the left
+			GetComponent<Rigidbody> ().velocity = new Vector3 (-speed, 0, 0);
+			// if the right button is down
+		} else if (Input.GetKey ("right")) {
+			// move the ball to the right
+			GetComponent<Rigidbody> ().velocity = new Vector3 (speed, 0, 0);
+		} else {
+			GetComponent<Rigidbody> ().velocity = new Vector3 (0, 0, 0);
+		}
+			
+	}
+
+	// when an object collides 
+	void OnTriggerEnter(Collider other) {
+		// if it is a enemy 
+		if (other.CompareTag ("Enemy")) {
+			Destroy (gameObject);
+		}
+		// destroy player
+	
+	}
+
+}
+
---
+++ Assets/SpawnController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnController : MonoBehaviour {
+
+	public GameObject enemy;
+	private float timeSinceLast;
+
+	void Start() {
+		timeSinceLast = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// if enough time has passed
+		if (timeSinceLast + 1.0f < Time.timeSinceLevelLoad) {
+			// spawn an enemy
+			GameObject enemyClone = (GameObject)Instantiate(enemy, new Vector3(transform.position.x + Random.Range(-5, 5), transform.position.y, transform.position.z), transform.rotation);
+			// move it forward 
+			enemyClone.GetComponent<Rigidbody>().velocity = new Vector3(0,0,-10);
+			// update
+			timeSinceLast = Time.timeSinceLevelLoad;
+		}
+
+	
+	}
+}
